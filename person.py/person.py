ad2101c7936b53101f18e94802724868f667fb33
Jason <99057964@mydavinci.nl>
2021-11-05 15:03:47 +0100

    Bezig met het maken van de lab: person.py

---
+++ person.py/person.py
@@ -0,0 +1,5 @@
+name = input('Type hier jou naam in: ')
+adress = input('Type hier jou adress in: ')
+postalCode = input('Type hier jou postcode in: ')
+residence = input('Type hier jou woonplaats in: ')
+
