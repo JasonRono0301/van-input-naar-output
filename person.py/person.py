7b4c113c38241a1494aa270d1c3b8742ac9c45b1
Jason <99057964@mydavinci.nl>
2021-11-08 12:00:14 +0100

    De lab af.
    En screenshots van gemaakt.

--- person.py/person.py
+++ person.py/person.py
@@ -3,3 +3,8 @@ adress = input('Type hier jou adress in: ')
 postalCode = input('Type hier jou postcode in: ')
 residence = input('Type hier jou woonplaats in: ')
 
+outputName = '| Naam  \t : ' + name
+outputAdress = '| Adress\t : ' + adress
+outputPostalcode = '| Postcode\t : ' + postalCode
+outputResidence = '| Woonplaats\t : ' + residence
+print(outputName + '\n' + outputAdress + '\n' + outputPostalcode + '\n' + outputResidence)
